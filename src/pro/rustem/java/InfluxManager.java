c1769fd16e07a25b17f69212e6524f953c0874db
Rustem Abdrakhmanov <rustemabd@gmail.com>
2018-05-08 09:24:05 +0300

    Initial commit

---
+++ src/pro/rustem/java/InfluxManager.java
@@ -0,0 +1,4 @@
+package pro.rustem.java;
+
+public class InfluxManager {
+}
---
+++ src/pro/rustem/java/InfluxParser.java
@@ -0,0 +1,26 @@
+package pro.rustem.java;
+import org.influxdb.*;
+import org.influxdb.InfluxDB.LogLevel;
+import org.influxdb.dto.BatchPoints;
+import org.influxdb.dto.BoundParameterQuery.QueryBuilder;
+import org.influxdb.dto.Point;
+import org.influxdb.dto.Pong;
+import org.influxdb.dto.Query;
+import org.influxdb.dto.QueryResult;
+import org.influxdb.dto.QueryResult.Series;
+import org.influxdb.impl.InfluxDBImpl;
+
+public class InfluxParser{
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!");
+        InfluxDB influxDB = InfluxDBFactory.connect("http://localhost:8086", "root", "root");
+        String dbName = "jmeter";
+        influxDB.setDatabase(dbName);
+        Query query = new Query("SELECT aggregate_report_rate FROM aggregateReports LIMIT 1", dbName);
+        influxDB.query(query, 20, queryResult -> System.out.println(queryResult));
+
+        influxDB.close();
+    }
+}
