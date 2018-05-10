f2e5618580c940dc47c6af825a0315b9286d9c51
Rustem Abdrakhmanov <rustemabd@gmail.com>
2018-05-10 12:04:21 +0300

    Pojo class for influxdb data fields added

--- src/main/java/pro/rustem/java/AgregateReport.java
+++ src/main/java/pro/rustem/java/AgregateReport.java
@@ -1,5 +1,24 @@
 package pro.rustem.java;
+import java.time.Instant;
 
 public class AgregateReport {
+    private Instant time;
+    private String aggregate_report_90_line;
+    private String aaggregate_report_95_line;
+    private String aggregate_report_99_line;
+    private String aggregate_report_bandwidth;
+    private String aggregate_report_count;
+    private String aggregate_report_error;
+    private String aggregate_report_max;
+    private String aggregate_report_median;
+    private String aggregate_report_min;
     private String aggregate_report_rate;
+    private String aggregate_report_stddev;
+    private String average;
+    private String buildID;
+    private String envType;
+    private String projectName;
+    private String requestName;
+    private String testType;
+    private String version_id;
 }
