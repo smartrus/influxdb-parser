86073a4586241b44949d19843c28b0c32c697834
Rustem Abdrakhmanov <rustemabd@gmail.com>
2018-05-11 13:27:55 +0300

    Refactored InfluxDB connection test to follow the convention

--- src/test/java/pro/rustem/java/influxdb/InfluxDBManagerTest.java
+++ src/test/java/pro/rustem/java/influxdb/InfluxDBManagerTest.java
@@ -1,9 +1,9 @@
 package pro.rustem.java.influxdb;
 import org.junit.jupiter.api.*;
 
-public class InfluxDBConnectionTest {
+public class InfluxDBManagerTest {
     @Test
-    public void testInfluxDBConnection() throws Exception {
+    public void testConnection() throws Exception {
         InfluxDBManager influxDBManager = new InfluxDBManager();
         Boolean result = influxDBManager.connect();
         Assertions.assertTrue(result);
