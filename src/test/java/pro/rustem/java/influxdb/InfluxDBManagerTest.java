3c5ae8697cd24fd8d8292a711739e90f2981fc9d
Rustem Abdrakhmanov <rustemabd@gmail.com>
2018-05-15 11:29:05 +0300

    Removed InfluxDBConfig test

--- src/test/java/pro/rustem/java/influxdb/InfluxDBManagerTest.java
+++ src/test/java/pro/rustem/java/influxdb/InfluxDBManagerTest.java
@@ -2,29 +2,6 @@ package pro.rustem.java.influxdb;
 import org.influxdb.InfluxDB;
 import org.junit.jupiter.api.*;
 
-class InfluxDBConfigTest {
-    @Test
-    void getUrlTest() {
-        InfluxDBConfig dbConfig = new InfluxDBConfig("http://test.com", "user", "password");
-        String url = dbConfig.getUrl();
-        Assertions.assertTrue(url.equals("http://test.com"));
-    }
-
-    @Test
-    void getUserTest() {
-        InfluxDBConfig dbConfig = new InfluxDBConfig("http://test.com", "user", "password");
-        String url = dbConfig.getUser();
-        Assertions.assertTrue(url.equals("user"));
-    }
-
-    @Test
-    void getPasswordTest() {
-        InfluxDBConfig dbConfig = new InfluxDBConfig("http://test.com", "user", "password");
-        String url = dbConfig.getPassword();
-        Assertions.assertTrue(url.equals("password"));
-    }
-}
-
 class InfluxDBManagerTest {
 
     @Test
